9df5cecb96639a21a86777957572b6b393d86464
Ali <muhammadaliqorbanzadeh@gmail.com>
2023-04-20 22:23:48 +0300

    Remove unused comments

--- src/components/result-header.component.tsx
+++ src/components/result-header.component.tsx
@@ -1,22 +1,17 @@
 function ResultHeader() {
   return (
-    // bg-gradient-to-b from-purple-600 to-blue-500
     <div className="w-full sm:rounded-[2rem] rounded-b-[2rem] bg-gradient-to-t from-gradient_light_royal_blue to-gradient_light_slate_blue py-4">
       <h1 className="text-xl font-semibold text-center opacity-70">
         Your Result
       </h1>
-      {/* cricle container */}
       <div className="flex items-center justify-center mt-8">
-        {/* circle */}
         <div className="p-8 rounded-full bg-gradient-to-b from-gradient_violet_blue to-gradient_persian_blue w-36 h-36">
           <div className="text-5xl font-bold text-center ">76</div>
           <div className="text-lg font-medium text-center opacity-50">
             of 100
           </div>
         </div>
-        {/*  */}
       </div>
-      {/*  */}
       <div className="flex flex-col items-center justify-center my-8">
         <div className="text-2xl font-bold text-center ">Great</div>
         <div className="px-4 mt-2 text-lg font-medium text-center opacity-80">
